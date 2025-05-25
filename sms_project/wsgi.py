94d0491ab20b086176a28cdaffa560e512531666
farisgozi <fariscraft709@gmail.com>
2025-05-26 01:26:28 +0700

    terdddd banggetz

--- sms_project/wsgi.py
+++ sms_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms_project.settings')
 
 application = get_wsgi_application()
+
+# This is the important part for Vercel deployment
+app = application
