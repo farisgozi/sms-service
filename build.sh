6f201b26ea9081771edbd1da1520468fd4472061
farisgozi <fariscraft709@gmail.com>
2025-05-26 01:19:32 +0700

    twokkk banggetz

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Menjalankan migrasi database
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run database migrations
 python manage.py migrate
 
-# Membuat superuser secara otomatis
+# Create superuser if not exists
 echo "from users.models import User; User.objects.create_superuser('admin', 'hexanest@gmail.com', 'hexanestdotaidi') if not User.objects.filter(email='hexanest@gmail.com').exists() else None" | python manage.py shell
